ede01fdad4a1c37a27380fac18d8256966eb2c16
ChainMasterTR - Mustafa SARI <musxos.2005@gmail.com>
2024-03-14 18:03:09 +0300

    0

---
+++ mint.ts
@@ -0,0 +1,36 @@
+import { percentAmount, generateSigner, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi'
+import { TokenStandard, createAndMint } from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import  "@solana/web3.js";
+import secret from './guideSecret.json';
+
+const umi = createUmi('https://capable-solemn-knowledge.solana-devnet.quiknode.pro/087b567de66d1aa4af8f58a55b689224439753be/'); //Replace with your QuickNode RPC Endpoint
+
+const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secret));
+const userWalletSigner = createSignerFromKeypair(umi, userWallet);
+
+const metadata = {
+    name: "Count DRACULA",
+    symbol: "DRACULA",
+    uri: "https://bafybeie6hhrmaxzvoutfigxcc5wiv4rbvuqzcfxubwm2fzhx5txusfwr3e.ipfs.nftstorage.link/metadata.json",
+};
+
+const mint = generateSigner(umi);
+umi.use(signerIdentity(userWalletSigner));
+umi.use(mplCandyMachine())
+
+createAndMint(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 8,
+    amount: 100000000000_00000000,
+    tokenOwner: userWallet.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi).then(() => {
+    console.log("Successfully minted 1 million tokens (", mint.publicKey, ")");
+});
